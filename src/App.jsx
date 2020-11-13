0691d695a1f64c3ba965558f6ab215bf91c908c5
Pachomen <julianalbertop7@gmail.com>
2020-11-13 18:34:10 -0500

    Api implement, font-style change, rating implement and final color background

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,33 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios";
 import Loading from "./Components/Loading";
+import Rating from "./Rating"
 
 function App() {
   const [appState, setAppState] = useState({
     loading: false,
-    img: null
+    img: null,
+    title: null
   });
 
   useEffect(() => {
     setAppState({ loading: true });
-    const url = "https://xkcd.com/620/info.0.json";
+    let rand = Math.floor(Math.random() * 600);
+    let url = "https://xkcd.com/"+rand+"/info.0.json";
     axios.get(url).then((respond) => {
-      const apiImg = respond.img;
-      setAppState({ loading: false, img: apiImg });
+      const apiImg = respond.data.img;
+      const apiTitle = respond.data.title;
+      setAppState({ loading: false, img: apiImg, title: apiTitle });
     });
   }, [setAppState]);
 
   return (
     <div className="App">
-      <h1></h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <img src={appState.img} alt="comict" width="500" height="600"></img>
+      <h1 >{appState.title}</h1>
+      <img src={appState.img} alt="comic"></img>
+      <div className="rate">
+      <Rating />
+      </div>
     </div>
   );
 }
